2be1cf6f2ca2ee6019a2c140a6d5325852e5685e
João D. Ferreira <jotomicron@gmail.com>
2020-08-18 23:17:51 +0100

    Improve the test code

--- .docker/docker-test.sh
+++ .docker/docker-test.sh
@@ -1,8 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 DOCKER_COMPOSE_DIR="$(dirname $0)"
 DOCKER_COMPOSE_FILE="$DOCKER_COMPOSE_DIR/docker-compose.yml"
 DOCKER_COMPOSE="docker-compose -f $DOCKER_COMPOSE_FILE --project-directory $DOCKER_COMPOSE_DIR"
+
+function ok() {
+    echo -e "\e[32;1m${1:-OK}\e[0m"
+}
+
+function error() {
+    echo -e "\e[31;1m${1:-ERROR}\e[0m"
+}
+
+function assert() {
+    if [ "$?" = 0 ]; then
+        ok
+    else
+        error
+    fi
+}
 
 function container() {
     container="$1"
@@ -10,38 +26,43 @@ function container() {
     echo -e "\nTesting container \e[1m$container\e[0m"
 }
 
-function assert() {
-    if [ "$1" = true ]; then
-        echo -e "\e[32;1mOK\e[0m"
-    else
-        echo -e "\e[31;1mERROR\e[0m"
-    fi
-}
-
 function docker_exec() {
-    $DOCKER_COMPOSE exec "$container" "$@"
+    extra=""
+
+    while [[ $1 =~ ^- ]]; do
+        extra="$extra $1"
+        shift
+    done
+
+    $DOCKER_COMPOSE exec $extra "$container" "$@"
 }
 
 function test_container_is_running() {
     local result="false"
-    echo "$(docker ps --filter name="$container")" | grep -q "$container" && result="true"
+
     echo -n "  container is running? "
-    assert $result
+
+    echo "$(docker ps --filter name="$container")" | grep -q "$container"
+    assert
 }
 
 function test_host_docker_internal() {
     local result="false"
-    docker_exec getent hosts "host.docker.internal" | grep -q "host.docker.internal" && result="true"
+
     echo -n "  can reach 'host.docker.internal'? "
-    assert $result
+
+    docker_exec getent hosts "host.docker.internal" | grep -q "host.docker.internal"
+    assert
 }
 
 function test_php_version() {
     expected="$1"
     local result="false"
-    docker_exec php -v | grep -q "PHP $expected" && result="true"
+
     echo -n "  php version is '$expected'? "
-    assert $result
+
+    docker_exec php -v | grep -q "PHP $expected"
+    assert
 }
 
 function test_php_modules() {
@@ -52,46 +73,57 @@ function test_php_modules() {
 
 function test_php_module() {
     local result="false"
-    docker_exec php -m | grep -q "$module" && result="true"
+
     echo -n "  has PHP module '$module'? "
-    assert $result
+
+    docker_exec php -m | grep -q "$module"
+    assert
 }
 
 function test_composer_is_installed() {
     local result="false"
-    docker_exec composer --version > /dev/null 2> /dev/null && result="true"
+
     echo -n "  has composer installed? "
-    assert $result
+
+    docker_exec composer --version >/dev/null 2>/dev/null
+    assert
 }
 
 function test_node_is_installed() {
     local result="false"
-    docker_exec bash --login -c 'node --version' > /dev/null 2> /dev/null && result="true"
+
     echo -n "  has node installed? "
-    assert $result
+
+    docker_exec --user=www-data bash --login -c 'node --version' >/dev/null 2>/dev/null
+    assert
 }
 
 function test_request_nginx() {
     local result="false"
-    curl -s "$1" | grep -q "$2" && result="true"
+
     echo -n "  can receive HTTP requests? "
-    assert $result
+
+    curl -s "$1" | grep -q "$2"
+    assert
 }
 
 function test_database_is_working() {
     local result="false"
-    docker_exec bash -c \
-        'mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" -e "SELECT 1" > /dev/null 2> /dev/null' \
-        && result="true"
+
     echo -n "  is running the database? "
-    assert $result
+
+    docker_exec bash -c \
+        'mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" -e "SELECT 1" > /dev/null 2> /dev/null'
+    assert
 }
 
 function test_php_can_access_db() {
     local result="false"
-    docker_exec curl -s "$1" | grep -q "$2" && result="true"
+
     echo -n "  can reach the database? "
-    assert $result
+
+    docker_exec curl -s "$1" | grep -q "$2"
+    assert
 }
 
 container "db"
